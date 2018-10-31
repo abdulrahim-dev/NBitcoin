bbdc40f0180a061f5d449348dc28f8760436d795
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2018-10-31 17:55:18 +0530

    Removed unwanted fields

--- BitCoinNew/BitCoinNew/Home.Designer.cs
+++ BitCoinNew/BitCoinNew/Home.Designer.cs
@@ -28,26 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblAddress = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtPublicKey = new System.Windows.Forms.TextBox();
-            this.txtPrivateKey = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnCreate = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lblAddress
-            // 
-            this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(270, 236);
-            this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(0, 13);
-            this.lblAddress.TabIndex = 11;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(185, 195);
+            this.label2.Location = new System.Drawing.Point(185, 164);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 10;
@@ -55,26 +44,10 @@
             // 
             // txtPublicKey
             // 
-            this.txtPublicKey.Location = new System.Drawing.Point(273, 195);
+            this.txtPublicKey.Location = new System.Drawing.Point(273, 164);
             this.txtPublicKey.Name = "txtPublicKey";
             this.txtPublicKey.Size = new System.Drawing.Size(412, 20);
             this.txtPublicKey.TabIndex = 9;
-            // 
-            // txtPrivateKey
-            // 
-            this.txtPrivateKey.Location = new System.Drawing.Point(273, 155);
-            this.txtPrivateKey.Name = "txtPrivateKey";
-            this.txtPrivateKey.Size = new System.Drawing.Size(412, 20);
-            this.txtPrivateKey.TabIndex = 8;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 163);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Private Key";
             // 
             // btnCreate
             // 
@@ -91,11 +64,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(892, 487);
-            this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPublicKey);
-            this.Controls.Add(this.txtPrivateKey);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCreate);
             this.Name = "Home";
             this.Text = "Home";
@@ -105,12 +75,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblAddress;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPublicKey;
-        private System.Windows.Forms.TextBox txtPrivateKey;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCreate;
     }
 }
